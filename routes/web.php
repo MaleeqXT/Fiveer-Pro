a5e57a46734d73dc997e2e5c2ed8cc9c03868bd5
MALEEQXT <maleeqxjuttyrt@gmail.com>
2024-11-12 20:54:54 +0500

    add fiveer admin/dashboard
    hsadsah

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FiveerController;
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/dashboard', [FiveerController::class, 'index'])->name('admin.dashboard');

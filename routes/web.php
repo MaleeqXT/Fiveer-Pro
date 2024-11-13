f9e62571bcd8cc31c507fa85e116c36259ffa185
MALEEQXT <maleeqxjuttyrt@gmail.com>
2024-11-13 21:43:41 +0500

    pull

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,16 @@ use App\Http\Controllers\FiveerController;
 
 
 
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/admin', [FiveerController::class, 'index'])->name('admin.dashboard');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

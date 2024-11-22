d737517b33839cec6aaf47e25970247c8ba34a3c
Hasnain <zainsaqain@gmail.com>
2024-11-22 10:08:37 -0800

    get start complete

--- resources/views/websites/complete.blade.php
+++ resources/views/websites/complete.blade.php
@@ -125,9 +125,123 @@
         </div>
         <label>
             <input type="radio" name="business_type" value="other"> Other business type
-          </label>
+          </label><br><br>
           
-        
+          
+          <p>How many people work for your business?</p>
+
+            <label>
+            <input type="radio" name="business_size" value="just_me"> Just me
+            </label><br>
+
+            <label>
+            <input type="radio" name="business_size" value="2_10"> 2 - 10
+            </label><br>
+
+            <label>
+            <input type="radio" name="business_size" value="11_50"> 11 - 50
+            </label><br>
+
+            <label>
+            <input type="radio" name="business_size" value="51_500"> 51 - 500
+            </label><br>
+
+            <label>
+            <input type="radio" name="business_size" value="500_plus"> 500+
+            </label><br><br><br>
+
+
+            <div class="mb-3">
+                <h6>What’s your business industry?</h6>
+                <input type="text" class="form-control" placeholder="Enter your industry">
+            </div><br><br><br>
+
+
+
+            <h6>What stage is your business in?</h6>
+
+            <div class="container">
+                <div class="row">
+                    <!-- First Row (2 Cards) -->
+                    <div class="col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h6>Idea</h6>
+                                <label class="form-check-label" for="sideHustle">
+                                    Planning your first steps.
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+            
+                    <div class="col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h6>Launch</h6>
+                                <label class="form-check-label" for="sideHustle">
+                                    Starting off or starting to sell.
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+                <div class="row">
+                    <!-- Second Row (2 Cards) -->
+                    <div class="col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h6>Expansion</h6>
+                                <label class="form-check-label" for="sideHustle">
+                                    Scaling and growing.
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+            
+                    <div class="col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h6>Established</h6>
+                                <label class="form-check-label" for="sideHustle">
+                                    Stable and achieved success.
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+
+            <p>How often does your business need freelance services?</p>
+
+            <label>
+              <input type="radio" name="frequency" value="weekly"> Weekly
+            </label><br>
+          
+            <label>
+              <input type="radio" name="frequency" value="monthly"> Monthly
+            </label><br>
+          
+            <label>
+              <input type="radio" name="frequency" value="quarterly"> Quarterly
+            </label><br>
+          
+            <label>
+              <input type="radio" name="frequency" value="twice_a_year"> Twice a year
+            </label><br>
+          
+            <label>
+              <input type="radio" name="frequency" value="yearly"> Yearly
+            </label><br>
+          
+            <label>
+              <input type="radio" name="frequency" value="not_sure"> I’m not sure
+            </label>
+
+            <button type="button" class="btn btn-dark" style="margin-left: 70%">Save</button>
+
+
     </div>
 </div>
 
